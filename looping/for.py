d8e3fe2ed52bc569cbb0cf840d6e2cd15375584f
hakimlutfi46 <lutfihakim1606@gmail.com>
2024-03-05 10:24:27 +0700

    looping for and whille

---
+++ looping/for.py
@@ -0,0 +1,5 @@
+for i in range(1,9):
+    if i % 2 == 0:  
+        if i >= 4:    
+            print(i)
+ 
---
+++ looping/while.py
@@ -0,0 +1,6 @@
+i = 10
+while i > 4:
+     if i == 5:
+          i = 10
+     i -= 1
+     print(i)
